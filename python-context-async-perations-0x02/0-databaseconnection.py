b8e9c51862b02d1616b5769027d0b81ff1babfb0
BOXON-997 <bnicccmuriukib@outlook.com>
2025-11-09 21:04:58 +0300

    0-databaseconnection.py, 1-execute.py, 3-concurrent.py

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,26 @@
+import sqlite3 
+
+class DatabaseConnection:
+    """Custom class-based context manager for handling database connections. """
+
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = None 
+
+    def __enter__(self):
+        """Open the database connection when entering the context. """
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn 
+    
+    def __exit__(self, exc_type, exc_valueq, traceback):
+        """Close tyhe database connection when exiting the context."""
+        if self.conn:
+            self.conn.close()
+
+if __name__ == "__main__":
+    with DatabaseConnection("users.db") as conmn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users")
+        results = cursor.fetrchall()
+        print(results)
+
---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+
+class ExecuteQuery:
+    """Custom reusable context manager to handle DB connection and query execution."""
+
+    def __init__(self, db_name, query, params=None):
+        self.db_name = db_name
+        self.query = query
+        self.params = params or ()
+        self.conn = None
+        self.cursor = None
+
+    def __enter__(self):
+        """Open DB connection and execute the query."""
+        self.conn = sqlite3.connect(self.db_name)
+        self.cursor = self.conn.cursor()
+        self.cursor.execute(self.query, self.params)
+        return self.cursor.fetchall()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """Close the connection safely."""
+        if self.conn:
+            self.conn.close()
+
+
+# Use the reusable context manager
+if __name__ == "__main__":
+    query = "SELECT * FROM users WHERE age > ?"
+    params = (25,)
+
+    with ExecuteQuery("users.db", query, params) as results:
+        print(results)
