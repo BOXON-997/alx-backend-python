cbd7e9b7cccc53e990445ea3d299d276960f617f
BOXON-997 <bnicccmuriukib@outlook.com>
2025-11-09 21:08:40 +0300

    updated 0-databaseconnection.py, 1-execute.py, 3-concurrent.py

--- python-context-async-perations-0x02/1-execute.py
+++ python-context-async-perations-0x02/1-execute.py
@@ -31,3 +31,11 @@ if __name__ == "__main__":
 
     with ExecuteQuery("users.db", query, params) as results:
         print(results)
+
+# Objective: create a reusable context manager that takes a query as input and executes it, managing both connection and the query execution
+
+# Instructions:
+
+#     Implement a class based custom context manager ExecuteQuery that takes the query: ”SELECT * FROM users WHERE age > ?” and the parameter 25 and returns the result of the query
+
+#     Ensure to use the__enter__() and the __exit__() methods
