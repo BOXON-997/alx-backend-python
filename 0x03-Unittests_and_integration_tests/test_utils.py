3bf4f630a514b7c5991d29b98c0df7e68d11e629
BOXON-997 <bnicccmuriukib@outlook.com>
2025-11-16 15:18:24 +0300

    Updated the test_utils file

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,3 +16,30 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_msg):
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+
+        self.assertEqual(str(cm.exception), f"'{expected_msg}'")
+
+class TestGetJson(unittest.TestCase):
+    """Test the get_json function"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """Test that get_json returns the expected payload"""
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+
+        with patch("utils.requests.get", return_value=mock_response) as mock_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            mock_get.assert_called_once_with(test_url)
+
