617fd2c77f0d6623dca0af926d93ec20e5b0d274
BOXON-997 <bnicccmuriukib@outlook.com>
2025-11-16 11:12:56 +0300

    Updated rest framework Authentication settings

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -49,9 +49,14 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES" : [
-        "rest_framework.permissions.AllowAny",
-    ]
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES" : [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ],
 }
+
 
 AUTH_USER_MODEL = "accounts.User"
 
